b0c439917a8e94276577dd961c1f948e99e12921
Prashant <Prashantrandiwe@gamil.com>
2023-09-21 00:28:18 +0530

    initial commit

---
+++ Crud/crud_operation/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import user
+
+class adding_data(forms.ModelForm):
+    class Meta:
+        model=user
+        fields=['name','email','password']
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'password':forms.PasswordInput(attrs={'class':'form-control'}),
+        }
---
+++ Crud/crud_operation/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .models import user
+from .forms import adding_data
+
+def registration(request):
+    stud=user.objects.all()
+    if request.method=='POST':
+        fm=adding_data(request.POST)
+        if fm.is_valid():
+            nm=fm.cleaned_data['name']
+            em=fm.cleaned_data['email']
+            pw=fm.cleaned_data['password']
+            reg=user(name=nm,email=em,password=pw)
+            reg.save()
+            
+    else:
+        fm=adding_data()
+    return render(request,'addshow.html',{"form":fm,'stu':stud})
+
+def delete(request,id):
+    if request.method=='POST':
+        pi=user.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
+
+def update_data(request,id):
+    if request.method=="POST":
+        pi=user.objects.get(pk=id)
+        fm=adding_data(request.POST,instance=pi)
+        if fm.is_valid():
+            fm.save()
+        return HttpResponseRedirect('/')
+    else:
+        pi=user.objects.get(pk=id)
+        fm=adding_data(instance=pi)
+    return render(request,'update.html',{"form":fm})
